5e6ce22d93266e8da64de4d86d90075deebbf00a
DeBugMarino <159265842+DeBugMarino@users.noreply.github.com>
2025-03-21 12:36:22 +0100

    add interessi e privacy

--- login-s/src/form.jsx
+++ login-s/src/form.jsx
@@ -1,27 +1,29 @@
 import { useState, useRef } from "react";
 
 export default function Form() {
-  const femminaRef = useRef(null)
-  const maschioRef = useRef(null)
-  const altroRef = useRef(null)
+  const femminaRef = useRef(null);
+  const maschioRef = useRef(null);
+  const altroRef = useRef(null);
 
-  
   const [message, setMessage] = useState("");
   const [data, setData] = useState({
-    id:"",
+    id: "",
     nome: "",
     cognome: "",
     email: "",
     password: "",
     genere: "",
     eta: "",
-    interessi:"",
+    interessi: "",
     privacy: false,
   });
   function handleChange(event) {
     setData({
       ...data,
-      [event.target.name]: event.target.name === "eta" ? parseInt(event.target.value) : event.target.value,
+      [event.target.name]:
+        event.target.name === "eta"
+          ? parseInt(event.target.value)
+          : event.target.value,
     });
   }
   function validate(password) {
@@ -47,11 +49,11 @@ export default function Form() {
       );
       return;
     }
-    if(data.eta <18){
-      setMessage("I minorenni non possono registrarsi")
+    if (data.eta < 18) {
+      setMessage("I minorenni non possono registrarsi");
       return;
-    }  
-    
+    }
+
     const updateUsers = [...databaseUsers, data];
     localStorage.setItem("users", JSON.stringify(updateUsers));
     setMessage("registrazione effetuata con successo");
@@ -62,69 +64,120 @@ export default function Form() {
       password: "",
     });
   }
- 
-  return (
- <>   
-    <form onSubmit={handleSubmit}>
-      <h6>Registrati</h6>
-      <label>Nome</label>
-      <input
-        required
-        type="text"
-        name="nome"
-        placeholder="Inserisci il tuo nome"
-        onChange={handleChange}
-        value={data.nome}
-      />
-      <label>Cognome</label>
-      <input
-        required
-        type="text"
-        name="cognome"
-        placeholder="Inserire cognome"
-        onChange={handleChange}
-        value={data.cognome}
-      />
-      <label>Email</label>
-      <input
-        required
-        type="email"
-        name="email"
-        placeholder="Inserire email"
-        onChange={handleChange}
-        value={data.email}
-      />
-      <label>Password</label>
-      <input
-        required
-        type="password"
-        name="password"
-        placeholder="Password"
-        onChange={handleChange}
-        value={data.password}
-      />
-      <section className="input-genere">
-      <p>Genere</p>
-      <div>
-      <input type="radio" id="femmina" name="genere" value="femmina" ref={femminaRef}  onChange={handleChange} />
-      <label htmlFor="F">Femmina</label>
-      </div>
-       <div>
-      <input type="radio" id="maschio" name="genere" value="maschio" ref={maschioRef}  onChange={handleChange}  />
-      <label htmlFor="maschio">Maschio</label>
-      </div>
-       <div>
-      <input type="radio" id="altro" name="genere" value="altro" ref={altroRef}  onChange={handleChange} />
-      <label htmlFor="altro">Altro</label>
-      </div>
-      </section>
-      <label>Età</label>
-      <input type="number" name="eta" value={data.eta} onChange={handleChange} required min={18}/>
-      
 
-      <button type="submit">Registrati</button>
-      {message && <p>{message}</p>}
-    </form>
-</>   
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h6>Registrati</h6>
+        <label>Nome</label>
+        <input
+          required
+          type="text"
+          name="nome"
+          placeholder="Inserisci il tuo nome"
+          onChange={handleChange}
+          value={data.nome}
+        />
+        <label>Cognome</label>
+        <input
+          required
+          type="text"
+          name="cognome"
+          placeholder="Inserire cognome"
+          onChange={handleChange}
+          value={data.cognome}
+        />
+        <label>Email</label>
+        <input
+          required
+          type="email"
+          name="email"
+          placeholder="Inserire email"
+          onChange={handleChange}
+          value={data.email}
+        />
+        <label>Password</label>
+        <input
+          required
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+          value={data.password}
+        />
+        <section className="input-genere">
+          <p>Genere</p>
+          <div>
+            <input
+              type="radio"
+              id="femmina"
+              name="genere"
+              value="femmina"
+              ref={femminaRef}
+              onChange={handleChange}
+            />
+            <label htmlFor="F">Femmina</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="maschio"
+              name="genere"
+              value="maschio"
+              ref={maschioRef}
+              onChange={handleChange}
+            />
+            <label htmlFor="maschio">Maschio</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="altro"
+              name="genere"
+              value="altro"
+              ref={altroRef}
+              onChange={handleChange}
+            />
+            <label htmlFor="altro">Altro</label>
+          </div>
+        </section>
+        <label>Età</label>
+        <input
+          type="number"
+          name="eta"
+          value={data.eta}
+          onChange={handleChange}
+          required
+          min={18}
+        />
+        <section className="interesse">
+          <label for="interessi-select">scegli un interesse</label>
+
+          <select name="interessi" id="select_interessi">
+            <option value="">--scegli un interesse--</option>
+            <option value="tecnologia">tecnologia</option>
+            <option value="viaggi">viaggi</option>
+            <option value="sport">sport</option>
+            <option value="musica">musica</option>
+            <option value="animali">animali</option>
+            <option value="videogiochi">videogiochi</option>
+          </select>
+        </section>
+
+        <section>
+          <label for="privacy">Accetta la privacy</label>
+          <input
+            type="checkbox"
+            id="privacy"
+            name="Policy"
+            value="Privacy Policy"
+            required
+          />
+        </section>
+
+        <button type="submit">Registrati</button>
+        {message && <p>{message}</p>}
+      </form>
+    </>
   );
 }
