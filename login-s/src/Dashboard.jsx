bc5d6e762ee6a124b915be30329b84f6e16c19d1
DeBugMarino <159265842+DeBugMarino@users.noreply.github.com>
2025-03-24 12:06:40 +0100

    dashboard

--- login-s/src/Dashboard.jsx
+++ login-s/src/Dashboard.jsx
@@ -1,45 +1,60 @@
+import { useState } from "react";
+
 export default function Dashboard({ setIsLogged }) {
   function handleLogout() {
     localStorage.removeItem("UserLogged");
     setIsLogged(false);
   }
-const user = JSON.parse(localStorage.getItem("UserLogged"))
-console.log(user)
-const [modifica, setModifica] = useState(false);
-const [userMod, setUserMod] = useState(user)
+  const user = JSON.parse(localStorage.getItem("UserLogged"));
+  console.log(user);
+  const [modifica, setModifica] = useState(false);
+  const [userMod, setUserMod] = useState(user);
 
-function handleSave(){
+  function handleChange(event) {
+    setUserMod({
+      ...user,
+      [event.target.name]:
+        event.target.name === "value"
+          ? parseInt(event.target.value)
+          : event.target.value,
+    });
+  }
 
-}
+  function handleSave() {
+    const users = JSON.parse(localStorage.getItem("users"));
+    const userSave = users.find((x) => x.id === user.id);
+    const indice = users.findIndex((x) => x.id === userMod.id);
+    users.splice(indice, 1, userMod);
+    localStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("UserLogged", JSON.stringify(userMod));
+    setModifica(false);
+  }
 
   return (
-   <>
-    <div>
-      <p>Utente loggato</p>
-      <button onClick={handleLogout}>Logout</button>
-    </div>
-    {modifica ? (  <>
-            <input type="text" name="nome" value={user.nome} onChange={handleChange} />
-            <input type="text" name="cognome" value={user.cognome} onChange={handleChange} />
-            <input type="email" name="email" value={user.email} onChange={handleChange} />
-            <input type="number" name="eta" value={user.eta} onChange={handleChange} />
-            <input type="text" name="interessi" value={user.interessi} onChange={handleChange} />
-            <button onClick={handleSave}>Salva</button>
-          </>
-        ) : (
-          <div>
+    <>
+      <div>
+        <p>Utente loggato</p>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+      {modifica ? (
+        <>
+          <input type="text" name="nome" onChange={handleChange} />
+          <input type="text" name="cognome" onChange={handleChange} />
+          <input type="number" name="eta" onChange={handleChange} />
+          <input type="text" name="interessi" onChange={handleChange} />
+          <button onClick={handleSave}>Salva</button>
+        </>
+      ) : (
+        <div>
           <h3>Info Utente</h3>
           <p>Nome: {user.nome}</p>
           <p>Cognome: {user.cognome}</p>
           <p>Email: {user.email}</p>
           <p>Età: {user.eta}</p>
           <p>Interessi: {user.interessi}</p>
-          <button onClick={()=> setModifica(true)}>Modifica</button>
+          <button onClick={() => setModifica(true)}>Modifica</button>
         </div>
-        )
-        }
-   
-      
+      )}
     </>
   );
 }
