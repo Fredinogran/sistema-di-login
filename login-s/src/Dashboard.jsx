4622ee53fd6fdf7a8903ee5c4f286d3d4bd2f7b4
AnthonyFerrara2898 <anthonyferrara2898@gmail.com>
2025-03-24 10:02:21 +0100

    24/03/2025

--- login-s/src/Dashboard.jsx
+++ login-s/src/Dashboard.jsx
@@ -1,13 +1,26 @@
 export default function Dashboard({ setIsLogged }) {
   function handleLogout() {
-    localStorage.removeItem("isLogged");
+    localStorage.removeItem("UserLogged");
     setIsLogged(false);
   }
-
+const user = JSON.parse(localStorage.getItem("UserLogged"))
+console.log(user)
   return (
+   <>
     <div>
       <p>Utente loggato</p>
       <button onClick={handleLogout}>Logout</button>
     </div>
+    <div>
+      <h3>Info Utente</h3>
+      <p>Nome: {user.nome}</p>
+      <p>Cognome: {user.cognome}</p>
+      <p>Email: {user.email}</p>
+      <p>Età: {user.eta}</p>
+      <p>Interessi: {user.interessi}</p>
+    </div>
+      
+    </>
   );
+
 }
