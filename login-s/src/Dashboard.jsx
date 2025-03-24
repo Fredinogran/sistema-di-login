5f0d557197fb66fc31e293e28c2d62c160309f9a
DavideTosti <davidetosti2002@gmail.com>
2025-03-24 15:33:54 +0100

    aggiornata dashboard

--- login-s/src/Dashboard.jsx
+++ login-s/src/Dashboard.jsx
@@ -1,20 +1,118 @@
+import { useState } from "react";
+
 export default function Dashboard({ setIsLogged }) {
+  const user = JSON.parse(localStorage.getItem("userLogged"));
+  const [modifica, setModifica] = useState(false);
+  const [userMod, setUserMod] = useState(user);
+
   function handleLogout() {
     localStorage.removeItem("userLogged");
     setIsLogged(false);
   }
-  const user = JSON.parse(localStorage.getItem("userLogged"));
+
+  function handleModifica() {
+    setModifica(true);
+  }
+
+  function handleChange(event) {
+    setUserMod({
+      ...user,
+      [event.target.name]:
+        event.target.name === "eta"
+          ? parseInt(event.target.value)
+          : event.target.value,
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    const users = JSON.parse(localStorage.getItem("users"));
+    const index = users.findIndex((x) => x.id === userMod.id);
+    users.splice(index, 1, userMod);
+    localStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("userLogged", JSON.stringify(userMod));
+    setModifica(false);
+  }
   return (
     <div>
       <p>Utente loggato</p>
-      <div>
-        <p>Nome: {user.nome}</p>
-        <p>Cognome: {user.cognome}</p>
-        <p>Email: {user.email}</p>
-        <p>Sesso: {user.sesso}</p>
-        <p>Età: {user.eta}</p>
-        <p>Interesse: {user.interessi}</p>
-      </div>
+      {modifica ? (
+        <form onSubmit={handleSubmit}>
+          <h6>Registrati</h6>
+          <label>Nome</label>
+          <input
+            type="text"
+            name="nome"
+            placeholder="Inserisci il tuo nome"
+            onChange={handleChange}
+            value={user.nome}
+            required
+          />
+          <label>Cognome</label>
+          <input
+            type="text"
+            name="cognome"
+            placeholder="Inserire cognome"
+            onChange={handleChange}
+            value={user.cognome}
+            required
+          />
+
+          <label>Sesso</label>
+          <div required onChange={handleChange} value={user.sesso}>
+            <input
+              type="radio"
+              id="maschio"
+              name="sesso"
+              value={"Maschio"}
+              checked
+            />
+            <label htmlFor="maschio">Maschio</label>
+
+            <input type="radio" id="femmina" name="sesso" value={"Femmina"} />
+            <label htmlFor="femmina">Femmina</label>
+
+            <input type="radio" id="altro" name="sesso" value={"Altro"} />
+            <label htmlFor="altro">Altro</label>
+          </div>
+          <label>Età</label>
+          <input
+            type="number"
+            name="eta"
+            id="eta"
+            min="18"
+            placeholder="Inserire età"
+            onChange={handleChange}
+            value={user.eta}
+            required
+          />
+          <label>Interesse</label>
+          <select
+            name="interessi"
+            id="interessi"
+            onChange={handleChange}
+            value={user.interessi}
+            required
+          >
+            <option value="">--Scegli un'opzione--</option>
+            <option value="tecnologia">Tecnologia</option>
+            <option value="viaggi">Viaggi</option>
+            <option value="sport">Sport</option>
+            <option value="musica">Musica</option>
+          </select>
+
+          <button type="submit">Modifica dati</button>
+        </form>
+      ) : (
+        <div>
+          <p>Nome: {user.nome}</p>
+          <p>Cognome: {user.cognome}</p>
+          <p>Email: {user.email}</p>
+          <p>Sesso: {user.sesso}</p>
+          <p>Età: {user.eta}</p>
+          <p>Interesse: {user.interessi}</p>
+        </div>
+      )}
+      <button onClick={handleModifica}>Modifica dati</button>
       <button onClick={handleLogout}>Logout</button>
     </div>
   );
