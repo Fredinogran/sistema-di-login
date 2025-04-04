26b4a38546d13a0714793fdbab509b1713ee56bd
Martina Costantino <martinacostantino@yahoo.it>
2025-04-04 11:59:18 +0200

    authContext

---
+++ login-s/src/components/Registrazione.jsx
@@ -0,0 +1,3 @@
+export default function Registrazione(){
+    return <p>Reg</p>
+  }
---
+++ login-s/src/context/authContext.jsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext()
+export const useAuth = () => useContext(AuthContext)
+useEffect(()=> {localStorage.setItem("users", JSON.stringify(users))},[users])
+export default function AuthProvider({children}){
+    const [user, setUser]= useState(()=>{
+        const localUser = localStorage.getItem("user")
+        return localUser ? JSON.parse(localUser) : null
+    })
+    const [users, setUsers]= useState(()=>{
+        const localUsers = localStorage.getItem("users")
+        return localUsers ? JSON.parse(localUsers) : []
+    })
+    function login({ email, password}){
+      const userExist = users.find((user)=> user.email === email && user.password === password)
+      if(!userExist){
+        return {esito: false, messaggio: "credenziali errate"}
+      }
+      setUser(userExist)
+      localStorage.setItem("user", JSON.stringify(userExist))
+    }
+    function registrazione(userData){
+       const userExist = users.find((user)=> user.email === userData.email)
+       if(userExist){
+        return {esito: false, messaggio: "Email già registrata"}
+       }
+       setUsers((prev)=> [...prev, userData] )
+       
+    }
+    function logout(){
+    localStorage.removeItem("user")
+    setUser(null)
+    }
+     return (
+        <AuthContext.Provider value={{user, users, login, registrazione, logout }}>
+            {children}
+         </AuthContext.Provider>
+     )
+}
