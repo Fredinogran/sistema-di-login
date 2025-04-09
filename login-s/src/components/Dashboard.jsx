a8771b883d7228d9035ea0edd51ac574fb6c6cff
DeBugMarino <159265842+DeBugMarino@users.noreply.github.com>
2025-04-09 10:57:19 +0200

    aggiunto dashboard

--- login-s/src/components/Dashboard.jsx
+++ login-s/src/components/Dashboard.jsx
@@ -14,13 +14,52 @@ export default function Dashboard() {
 
   return (
     <div>
-      <p>Dash</p>
+      <h1 className="titolo">
+        <strong>Informazioni Personali:</strong>
+      </h1>
+      <hr></hr>
+      <br></br>
+      <div className="flow-root">
+        <dl className="-my-3 divide-y divide-gray-200 text-sm">
+          <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
+            <dt className="font-medium text-gray-900">Nome e Cognome</dt>
 
-      <p>Nome: {user.nome}</p>
-      <p>Cognome: {user.cognome}</p>
-      <p>Email: {user.email}</p>
+            <dd className="text-gray-700 sm:col-span-2">
+              {user.nome} {user.cognome}
+            </dd>
+          </div>
 
-      <button onClick={Handlelogout}>Logout</button>
+          <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
+            <dt className="font-medium text-gray-900">Email</dt>
+
+            <dd className="text-gray-700 sm:col-span-2">{user.email}</dd>
+          </div>
+
+          <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
+            <dt className="font-medium text-gray-900">Telefono</dt>
+
+            <dd className="text-gray-700 sm:col-span-2">{user.tel}</dd>
+          </div>
+
+          <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
+            <dt className="font-medium text-gray-900">
+              Indirizzo casa da visitare
+            </dt>
+
+            <dd className="text-gray-700 sm:col-span-2">{user.indirizzo}</dd>
+          </div>
+
+          <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
+            <dt className="font-medium text-gray-900">Data Appuntamento</dt>
+
+            <dd className="text-gray-700 sm:col-span-2">{user.data}</dd>
+          </div>
+        </dl>
+      </div>
+      <br></br>
+      <button className="logout" onClick={Handlelogout}>
+        Logout
+      </button>
     </div>
   );
 }
