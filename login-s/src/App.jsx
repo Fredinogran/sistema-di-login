5e4d77526e556c5d118998364711023735137020
Antonio Colella <antoniocolella80@gmail.com>
2025-04-09 10:00:53 +0200

    aggiunta header

--- login-s/src/App.jsx
+++ login-s/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/carrello" element={<Carrello/>}></Route>
+          <Route path="/carrello" element={<Carrello />}></Route>
           <Route path="/login" element={<Login></Login>}></Route>
           <Route
             path="/dashboard"
